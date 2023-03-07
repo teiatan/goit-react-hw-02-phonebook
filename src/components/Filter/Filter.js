389b05645cb307f66d5bb27adc7d8adb45787ea0
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-07 22:32:38 +0200

    filter class with input change handler function

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,11 +1,31 @@
 //import PropTypes from 'prop-types';
+import { Component } from "react";
 
-export function Filter({props}) {
-    return (
-        <>
-            
-        </>
-    );
+export class Filter extends Component {
+    state = {
+        filter: "",
+    };
+
+    handleInputChange = e => {
+        const { name, value } = e.currentTarget;
+        this.setState(
+            {[name]: value}
+        );  
+    };
+
+    render() {
+        return (
+            <label>
+                Find contacts by name
+                <input
+                    type="text"
+                    name="filter"
+                    value={this.state.value}
+                    onChange={this.handleInputChange}
+                />
+            </label>
+        );
+    } 
 };
 
 Filter.propTypes = {
