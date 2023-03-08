4451470657107a62162d52be5993437ddd845929
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-08 12:50:11 +0200

    delete not used code from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import { Component } from "react";
 import { Section } from "./Section/section";
 import { ContactForm } from "./ContactForm/ContactForm";
@@ -6,16 +5,6 @@ import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 
 export class App extends Component {
-  static defaultProps = {
-    initialContacts: [],
-    initialFilter: '',
-  };
-
-  static propTypes = {
-    initialContacts: PropTypes.array,
-    initialFilter: PropTypes.string,
-  };
-
   state = {
     contacts: [
       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
@@ -23,7 +12,6 @@ export class App extends Component {
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
-    filter: '',
     filterArray: '',
   };
 
@@ -36,10 +24,6 @@ export class App extends Component {
       window.alert(`${data.name} is already in contacts`);
     };
     
-  };
-
-  takeeDataFromFilterInput = data => {
-    this.setState({filter: data});
   };
 
   takeDataFromFilterInput = data => {
