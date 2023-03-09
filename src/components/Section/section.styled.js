7f9c42b4d754529b217085fd85627d724819dd37
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-09 02:01:57 +0200

    styles section components

---
+++ src/components/Section/section.styled.js
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const SectionEl = styled.section`
+    width: 600px;
+    margin: 0 auto;
+    padding-top: 15px;
+    padding-bottom: 15px;
+`;
+
+
+export const Title = styled.h2` 
+    width: 100%;
+    padding-top: 30px;
+    padding-bottom: 30px;
+    text-align: center;
+    margin: 0;
+    background-color: white;
+    color: darkslategray;
+    text-transform: uppercase;
+    border-radius: 6px;
+`;
+    
