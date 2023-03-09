662acab324a3106583fecb4326e860d7b6e5d2fc
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-09 23:52:06 +0200

    delete props for App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,20 @@ import { Container } from "./App.styled";
 
 export class App extends Component {
   state = {
-    contacts: this.props.innerContacts,
-    //filter: "",
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: "",
     filterArray: [],
-    renderArray: this.props.innerContacts,
+    renderArray: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
   };
 
   takeDataFromSubmitForm = data => {
