4a4e536b7339a20567f73c6b02998b8294e1c565
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-10 16:29:45 +0200

    change filter component from class type to function type

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,38 +1,21 @@
-//import PropTypes from 'prop-types';
-import { Component } from "react";
+import PropTypes from 'prop-types';
 import { Label, Input } from "./Filter.styled";
 
-export class Filter extends Component {
-    state = {
-        filter: "",
-    };
-
-    handleInputChange = e => {
-        const { name, value } = e.currentTarget;
-        this.setState( () => {
-            return  {[name]: value};
-        });
-        this.setState( prevState => {
-            this.props.filter(prevState.filter);
-        });
-    };
-
-
-    render() {
-        return (
-            <Label>
-                Find contacts by name
-                <Input
-                    type="text"
-                    name="filter"
-                    value={this.props.value}
-                    onChange={this.props.onChange}
-                />
-            </Label>
-        );
-    } 
+export function Filter ({value, onChange}) {
+    return (
+        <Label>
+            Find contacts by name
+            <Input
+                type="text"
+                name="filter"
+                value={value}
+                onChange={onChange}
+            />
+        </Label>
+    );
 };
 
 Filter.propTypes = {
-    
+    value: PropTypes.string, 
+    onChange: PropTypes.func,
 };
